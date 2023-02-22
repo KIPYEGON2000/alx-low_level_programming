8c2efb9371d79a992fdc2ab1045d732dda5f4d99
KIPYEGON2000 <60179260+KIPYEGON2000@users.noreply.github.com>
2023-02-22 19:17:57 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int j=0;
-	while(j<10)
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
 	{
-		char i;
-		for(i='a';i<='z';i++){
+		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
-			}
 		_putchar('\n');
-		j++;
-		}
+	}
 }
