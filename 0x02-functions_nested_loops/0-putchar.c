cd58c4b7a04cb5cc9261a1794d75a1e8f7f857c6
KIPYEGON2000 <kiptoamos.gmail.com>
2023-02-22 06:17:39 -0800

    function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
+#include <main.h>
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -10,4 +10,8 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+int main(void){
+	_putchar("_putchar");
+	return 0;
 }
