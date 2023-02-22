71c0c79946611c24d0c400801373756ab6c4af91
KIPYEGON2000 <60179260+KIPYEGON2000@users.noreply.github.com>
2023-02-22 21:49:45 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,13 @@
 #include "main.h"
+/**
+ *_abs - Check main
+ * @i: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number i
+ */
 
-int _abs(int){
-	int i;
+int _abs(int i){
+	
 	if(i<0){
 		i=(-1)*n;
 		return i;
