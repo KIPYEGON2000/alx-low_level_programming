a7da4c110940298bfbc6bcdeb5d5cb77d834e615
KIPYEGON2000 <60179260+KIPYEGON2000@users.noreply.github.com>
2023-02-22 19:47:23 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,12 +6,14 @@
  */
 int _isalpha(int c){
 	int i,j;
-	for(i='a';i<='z';i++ || i='A';i<='Z';i++){
+	for(i='a';i<='z';i++ ){
+		for(i='A';i<='Z';i++){
 		if(i=0){
 			_putchar("0";
 			_putchar('\n');}
 		else{ _putchar("1");
 		_putchar('\n');
+		    }
 		}
 		}
 		}
