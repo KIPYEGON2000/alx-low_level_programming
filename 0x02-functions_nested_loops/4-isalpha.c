affa8d00fb6b286080c3a9f64556320173ec7e24
KIPYEGON2000 <kiptoamos.gmail.com>
2023-02-22 08:39:08 -0800

    is 1 or 0

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c){
+	int i,j;
+	for(i='a';i<='z';i++ | i='A';i<='Z';i++){
+		if(i=0){
+			_putchar("0";
+			_putchar('\n');}
+		else{ _putchar("1");
+		_putchar('\n');
+		}
+		}
+		}
