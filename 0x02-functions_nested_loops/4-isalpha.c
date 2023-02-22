9afa93ed8e1a99c55c87d7326c62d6e53035dc8e
KIPYEGON2000 <60179260+KIPYEGON2000@users.noreply.github.com>
2023-02-22 19:49:34 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,16 +4,18 @@
  *
  * Return: Always 0.
  */
-int _isalpha(int c){
-	int i,j;
-	for(i='a';i<='z';i++ ){
-		for(i='A';i<='Z';i++){
-		if(i=0){
-			_putchar("0";
-			_putchar('\n');}
-		else{ _putchar("1");
-		_putchar('\n');
-		    }
+int _isalpha(int c)
+{
+	char lower, upper;
+	int isletter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+				isletter = 1;
 		}
-		}
-		}
+	}
+	return (isletter);
+}
